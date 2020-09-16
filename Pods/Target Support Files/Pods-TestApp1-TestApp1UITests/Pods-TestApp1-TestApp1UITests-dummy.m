94c97a0b4d3cf531d84d7471a1cfb641f51d0e08
TejaSree <teja.marthy@radisy.com>
2020-09-16 12:22:53 +0530

    Initial commit

---
+++ Pods/Target Support Files/Pods-TestApp1-TestApp1UITests/Pods-TestApp1-TestApp1UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestApp1_TestApp1UITests : NSObject
+@end
+@implementation PodsDummy_Pods_TestApp1_TestApp1UITests
+@end
---
+++ TestApp1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TestApp1
+//
+//  Created by Apple on 16/09/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchFilms()
+        // Do any additional setup after loading the view.
+    }
+
+   open func fetchFilms() {
+      // 1
+      let request = AF.request("https://swapi.dev/api/films")
+      // 2
+      request.responseJSON { (data) in
+        print(data)
+      }
+    }
+
+}
+
